1e287c2fac8822c8a43f0d730e7177b4810a7acb
Arya Pareek <aryapareek023@gmail.com>
2024-07-29 22:45:17 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -22,3 +22,22 @@ if (popup) {
     }, 1000);
   });
 }
+
+let cartCount = 0;
+
+function updateCartCount(event) {
+  const cartIcon = document.querySelector('.cart-icon');
+  const cartCountElement = cartIcon.querySelector('.cart-count');
+  cartCount++;
+  cartCountElement.textContent = cartCount;
+  cartIcon.classList.add('animate');
+  setTimeout(() => {
+    cartIcon.classList.remove('animate');
+  }, 500);
+}
+
+const productCartIcons = document.querySelectorAll('.product-item .icons li:last-child');
+
+productCartIcons.forEach(icon => {
+  icon.addEventListener('click', updateCartCount);
+});
